b07672ab44d509cd663d224260c53b34b51342d4
Alice-63 <alicetindag84@gmail.com>
2021-08-11 20:21:01 +0200

    add

--- script.js
+++ script.js
@@ -6,6 +6,18 @@ const app= new Vue
     el:"#app",
     data:
     {
-        name:"Ali"
+        name:"Ali",
+        treu:"true"
+    },
+    components:
+    {
+        test:
+        {
+            template:"<h1> Ali</h1>"
+        },
+        kesk:
+        {
+            template:"<h1> Veli</h1>"
+        }
     }
 })
