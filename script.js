da10b291447dc3b4e3dd852b1b5703cccc6f89da
Alice-63 <alicetindag84@gmail.com>
2021-08-11 18:45:50 +0200

    add

--- script.js
+++ script.js
@@ -1,23 +1,11 @@
 
 
 
-const app=new Vue
+const app= new Vue
 ({
     el:"#app",
-  data:
-  {
-      row:
-      [
-          {name:"",surname:""}
-      ]
-  },
-  methods:{
-    add(){
-        this.row.push({name:"",surname:""})
-    },
-    del(index){
-        this.row.splice(index,1)
+    data:
+    {
+        name:"Ali"
     }
-  }
-
 })
